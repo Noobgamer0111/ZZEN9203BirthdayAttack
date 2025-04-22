667c8c2ed624882cc3337962d8447e79780e9bfe
Aadil Syed <aadilsyed5117@gmail.com>
2025-04-22 13:36:47 +1000

    update HashBirthdayComparer.py

--- HashBirthdayComparer.py
+++ HashBirthdayComparer.py
@@ -10,12 +10,12 @@ import time, hashlib
 
 start_time = time.time()
 # Create real confession file
-with open('', 'w') as f:
-    f.write("This is the secret confession of Richard Buckland to be revealed by anonymous email if I should mysteriously vanish. I have left the last few hex digits of the SHA256 hash of this message with my trusted solicitor, Dennis Denuto, which will verify that this is indeed my intended and unaltered confession written by me Richard Buckland. Dennis has not seen this confession he has only seen the last few digits of the hash. I have also sent copies of the last few digits to my bank manager and to my priest Father Brown. On the 10th of February I saw Mark Zukerberg peeping through my window and recording my private and personal conversation with my friend. I confronted him and he was very embarrassed. He promised to pay me $1 million a year if I would stay silent and not tell anyone I had seen him do this. I agreed but now I worry that it would be cheaper for him to make me vanish than to keep paying me.\n")
+with open('aliceforpresident.pdf', 'w') as f:
+    f.write("My zid is z5263769,\n\nI believe that Alice will become President - and I'm pretty good at predicting things.")
 
 # Create fake confession file
-with open('fake_confession.txt', 'w') as f:
-    f.write("This is the secret confession of Richard Buckland to be revealed by anonymous email if I should mysteriously vanish. I have left the last few hex digits of the SHA256 hash of this message with my trusted solicitor, Dennis Denuto, which will verify that this is indeed my intended and unaltered confession written by me Richard Buckland. Dennis has not seen this confession he has only seen the last few digits of the hash. I have also sent copies of the last few digits to my bank manager and to my priest Father Brown. On the 10th of February I saw Mark Zukerberg near my house and we struck up a conversation. He explained all the things he was doing to ensure that Facebook respects privacy - both of its users and of others. It was very impressive. I feel awful that I have been criticising Facebook publicly for so long. I apologised to him in our conversation and now I want to confess to the world that actually Facebook has more than enough privacy features, and that the reasonI spend so much time criticising Facebook is that I am envious of Mark and wish I was a clever and smart and wise as he is. I feel so bad for having been so mean to him for so many years that I am considering retreating to the outback. I may well cut off all contact with the world and live as a hermit from now on. So do not worry if I vanish it is just that I feel so guilty that I have been so unfair to Facebook.\n")
+with open('bobforpresident.pdf', 'w') as f:
+    f.write("My zid is z5263769,\n\nI believe that Bob will become President - and I'm pretty good at predicting things.")
 
 # Calculate the SHA256 hash of the confession files.
 def get_sha256_last_digits(file_path,num_digits):
@@ -38,14 +38,14 @@ def modify_file_to_match_hash(real_file, fake_file, num_digits):
             break
 
 # Modify the fake confession file to match the hash of the real confession file
-modify_file_to_match_hash('real_confession.txt', 'fake_confession.txt', 3)
+modify_file_to_match_hash('real_confession.txt', 'fake_confession.txt', 2)
 
-real_hash = get_sha256_last_digits('real_confession.txt', 3)
-fake_hash = get_sha256_last_digits('fake_confession.txt', 3)
+real_hash = get_sha256_last_digits('real_confession.txt', 2)
+fake_hash = get_sha256_last_digits('fake_confession.txt', 2)
 
 # Print the last 3 digits of the real and fake confession hashes
-print(f"Real confession hash (last 3 digits): {real_hash}")
-print(f"Fake confession hash (last 3 digits): {fake_hash}")
+print(f"Real confession hash (last 2 digits): {real_hash}")
+print(f"Fake confession hash (last 2 digits): {fake_hash}")
 
 # Calculate the time taken to run the script
 end_time = time.time()
